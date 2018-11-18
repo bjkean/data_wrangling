01252418dd9dc4845acf3a989eb1a41960f5e79e
bjkean <bjkean@gmail.com>
2018-11-18 18:25:13 -0500

    assignment1

--- assignment1.R
+++ assignment1.R
@@ -22,13 +22,13 @@ names <- c('philips', 'akzo', 'van houten', 'unilever')
 unique(df$company)
 
 #getting number of unique values in company
-length(unique(df$company)) #19
+length(unique(df$company)) #19 unique values
 
 #converting all values to lowercase
 df$company <- tolower(df$company)
 
 unique(df$company)
-length(unique(df$company)) #12
+length(unique(df$company)) #12 unique values
 
 
 #set variables based on patterns in each
@@ -45,6 +45,36 @@ for (var in my_vars){
 #create lookup table
 lut <- c('ps'='philips', 'ak'='akzo','van'='van houten','uni'='unilever')
 df$company <- lut[df$company]
-df$company
 
+#Separate the product code and product number into separate columns
+df <- separate(df, `Product code / number`, c('product_code','product_number'),sep="-")
 
+lut2 <- c('p'='Smartphone' , 'v'='TV' , 'x'='Laptop' , 'q'='Tablet')
+
+df$product <- lut2[df$product_code]
+
+df <- unite(df, "full_address", c(address, city, country), sep=',',remove=FALSE)
+
+#creates binary cols for company name
+df$company_philips <- as.numeric(df$company=='philips')
+df$company_akzo <- as.numeric(df$company=='akzo')
+df$company_van_houten <- as.numeric(df$company=='van houten')
+df$company_unilever <- as.numeric(df$company=='unilever')
+
+#creates binary cols for product
+df$product_smartphone <- as.numeric(df$product=='Smartphone')
+df$product_tv <- as.numeric(df$product=='TV')
+df$product_laptop <- as.numeric(df$product=='Laptop') 
+df$product_tablet <- as.numeric(df$product == 'Tablet')
+
+##code not working, variable not recognizing, final output is column called 'column'
+cols <- c("company_philips", "company_akzo", "company_van_houten", "company_unilever")
+vals <- c('pilips','azko','van houten','unilever')
+
+num = 1
+
+for (column in cols){
+    df$column <- as.numeric(df$company==vals[num])
+    num < - num + 1
+  }
+View(df)
